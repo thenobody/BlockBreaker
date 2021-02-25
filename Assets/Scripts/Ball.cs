6dea704159edad5aa7b6d1ab98ce4b47b367ccdb
Anton Vanco <vanco.anton@gmail.com>
2021-02-25 10:50:16 +0100

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+  // config parameters
+  [SerializeField] Paddle paddle;
+  [SerializeField] Vector2 initialVelocity;
+
+  // state
+  private Vector2 paddleDistance;
+  // Start is called before the first frame update
+  void Start()
+  {
+    paddleDistance = transform.position - paddle.transform.position;
+    GetComponent<Rigidbody2D>().simulated = false;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetMouseButton(0))
+    {
+      GetComponent<Rigidbody2D>().simulated = true;
+      GetComponent<Rigidbody2D>().velocity = initialVelocity;
+    }
+    else if (!GetComponent<Rigidbody2D>().simulated)
+    {
+      Vector2 paddlePosition = paddle.transform.position;
+      transform.position = paddlePosition + paddleDistance;
+    }
+  }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    Camera camera = Camera.main;
+    Vector2 position = camera.ScreenToWorldPoint(Input.mousePosition);
+    float maxWidth = camera.orthographicSize * camera.aspect * 2;
+    float xNextPosition = Mathf.Clamp(position.x, 1, maxWidth - 1);
+    float yNextPosition = transform.position.y;
+
+    transform.position = new Vector2(xNextPosition, yNextPosition);
+  }
+}
